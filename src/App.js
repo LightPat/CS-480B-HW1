efcd16854ec871057a63eadec4d0315663a4e505
LightPat <60397186+LightPat@users.noreply.github.com>
2022-02-03 16:32:40 -0500

    reading privacy policy text file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from './pages/Home.js';
 import Dark from './pages/Dark.js';
 import Light from './pages/Light.js';
 import ErrorPage from './pages/ErrorPage.js';
 
 function App() {
   return (
-    <Router className='App'>
+    <Router>
       <nav className='Nav-bar'>
-        <Link to="/" className='Nav-element'>Home</Link>
-        <Link to="/dark" className='Nav-element'>Dark Version</Link>
         <Link to="/light" className='Nav-element'>Light Version</Link>
+        <Link to="/dark" className='Nav-element'>Dark Version</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dark" element={<Dark />} />
+        <Route path="/" />
         <Route path="/light" element={<Light />} />
+        <Route path="/dark" element={<Dark />} />
+        {/* <Route path="/privacypolicy" element={<PrivacyPolicy />} /> */}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
--- src/pages/Dark.js
+++ src/pages/Dark.js
@@ -1,11 +1,26 @@
 import React from "react";
+import privacypolicy from './Privacy Policy.txt';
+
+function readPrivacyPolicyFile() {
+  fetch(privacypolicy)
+  .then(response => response.text())
+  .then(data => {
+    const element = document.getElementById("privacy-policy")
+    element.innerHTML = data;
+    return data
+  })
+}
 
 function Dark() {
-    return (
-      <div>
-        <h2>Dark page</h2>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Dark page</h1>
 
-  export default Dark;
+      <p id="privacy-policy">{readPrivacyPolicyFile()}</p>
+    </div>
+  );
+}
+
+
+
+export default Dark;
